61f6768ae506ca9fabdde8d0c711b5d4572adcbe
vitthala27 <vitthalgole7@gmail.com>
2025-01-15 15:47:28 +0530

    kjbfb

--- javfile.java
+++ javfile.java
@@ -2,6 +2,6 @@ class javfile
 {
     public static void main(String[] args)
     {
-        System.out.println("My Second Java Program.");
+        System.out.println("My next to the  Java Program.");
     }
 }
