869932d86ff7a0c11efd253abd5c7c2357caa6da
vitthala27 <vitthalgole7@gmail.com>
2025-01-15 15:16:57 +0530

    second push

--- javfile.java
+++ javfile.java
@@ -2,6 +2,6 @@ class javfile
 {
     public static void main(String[] args)
     {
-        System.out.println("My First Java Program.");
+        System.out.println("My Second Java Program.");
     }
 }
